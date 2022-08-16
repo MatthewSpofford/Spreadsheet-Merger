da4b4ca809e338433532ff2bfb953adeb84cce5e
Matthew Spofford <matthewspofford@verizon.net>
2022-08-16 19:15:35 -0400

    Improved UI; started plans for merging algorithm

--- merger/gui.py
+++ merger/gui.py
@@ -1,66 +1,94 @@
+import os
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from tkinter.ttk import *
 
-from merger.merger import merge_spreadsheets
+import merger.merger as merger
+
+
+_initial_dir = "./"
 
 
 class MergerGUI(Frame):
 
-  def __init__(self, root):
-    super().__init__(root, padding=("20", "20", "20", "20"))
+    def __init__(self, root):
+        super().__init__(root, padding=("20", "20", "20", "20"))
 
-    self.columnconfigure(2, weight=1)
-    self.rowconfigure(2, weight=1)
+        self.columnconfigure(2, weight=1)
+        self.rowconfigure(2, weight=1)
 
-    self._main_select = SpreadsheetSelect(root, 0, "Original Spreadsheet:")
+        self._main_select = SpreadsheetSelect(root, 0, "Original Spreadsheet")
 
-    self._new_select = SpreadsheetSelect(root, 1, "New Spreadsheet:")
+        self._new_select = SpreadsheetSelect(root, 1, "New Spreadsheet")
 
-    self._merge_btn = Button(root, text="Merge", command=self.merge_spreadsheets)
-    self._merge_btn.grid(column=0, row=2, columnspan=3, padx=20, pady=10)
+        self._merge_btn = Button(root, text="Merge", command=self.merge_spreadsheets)
+        self._merge_btn.grid(column=0, row=2, columnspan=3, padx=20, pady=10)
 
-    for child in self.winfo_children():
-      child.grid_configure(padx=5, pady=5)
+        for child in self.winfo_children():
+            child.grid_configure(padx=5, pady=5)
 
-    self._main_select._file_path_entry.focus()
+        self._main_select._file_path_entry.focus()
 
-  def merge_spreadsheets(self):
-    try:
-      merge_spreadsheets(self._main_select.file_path,
-                         self._main_select.file_path)
-    except Exception as e:
-      print(e)
+    def merge_spreadsheets(self):
+        # Validate file selector input for all selections
+        selectors = [self._main_select, self._new_select]
+        for select in selectors:
+            select.file_path = select.file_path.strip()
 
-    from merger.__main__ import root
-    root.quit()
+            # Output error if file path is invalid and cancel merge
+            if not os.path.isfile(select.file_path):
+                messagebox.showerror("Merge Error",
+                                     "The file path given for the " + select.label_text.lower()
+                                     + " is not a valid path.")
+                return
+
+        # Begin spreadsheet merging process
+        try:
+            merger.merge_spreadsheets(self._main_select.file_path,
+                               self._new_select.file_path)
+        except Exception as e:
+            messagebox.showerror("Merge Error", str(e))
+
+        # Close app
+        from merger.__main__ import root
+        root.quit()
 
 
 class SpreadsheetSelect:
 
-  def __init__(self, root, row, label_text):
-    # super().__init__(root)
+    def __init__(self, root, row, label_text: str):
+        # super().__init__(root)
 
-    # self.columnconfigure(2, weight=1)
-    # self.rowconfigure(0, weight=1)
+        # self.columnconfigure(2, weight=1)
+        # self.rowconfigure(0, weight=1)
 
-    self._label = Label(root, text=label_text)
-    self._label.grid(column=0, row=row, padx=20, pady=15)
+        self.label_text = label_text
 
-    self._file_path = StringVar()
-    self._file_path_entry = Entry(root, textvariable=self._file_path)
-    self._file_path_entry.grid(column=1, row=row, padx=0, pady=15)
+        self._label = Label(root, text=self.label_text + ":")
+        self._label.grid(column=0, row=row, padx=20, pady=15)
 
-    self._select_btn = Button(root, text="Select...", command=self._select_file)
-    self._select_btn.grid(column=2, row=row, padx=20, pady=15)
+        self._file_path = StringVar()
+        self._file_path_entry = Entry(root, textvariable=self._file_path)
+        self._file_path_entry.grid(column=1, row=row, padx=0, pady=15)
 
-  def _select_file(self):
-    file_path = filedialog.askopenfilename(filetypes=[("Excel Spreadsheet", ".xlsx")],
-                                           title="Select Spreadsheet",
-                                           initialdir="./")
-    self._file_path.set(file_path)
+        self._select_btn = Button(root, text="Select...", command=self._select_file)
+        self._select_btn.grid(column=2, row=row, padx=20, pady=15)
 
-  @property
-  def file_path(self):
-    return self._file_path.get()
+    def _select_file(self):
+        global _initial_dir
+        file_path = filedialog.askopenfilename(filetypes=[("Excel Spreadsheet", ".xlsx")],
+                                               title="Select Spreadsheet",
+                                               initialdir=_initial_dir)
 
+        # Save initial dir in the parent directory of this newly selected file
+        _initial_dir = os.path.dirname(file_path)
+
+        self._file_path.set(file_path)
+
+    @property
+    def file_path(self):
+        return self._file_path.get()
+
+    @file_path.setter
+    def file_path(self, file_path):
+        self._file_path.set(file_path)
--- merger/merger.py
+++ merger/merger.py
@@ -1,4 +1,14 @@
+import openpyxl as pyxl
 
 
-def merge_spreadsheets(main_file, new_file):
-  pass
+def merge_spreadsheets(main_file_path, new_file_path):
+    main_sheet = pyxl.load_workbook(main_file_path)["Manager_Opportunity_Dashboard"]
+    new_sheet = pyxl.load_workbook(new_file_path, read_only=True)["Manager_Opportunity_Dashboard"]
+
+    # Map column labels used in new sheet to the main sheet positions
+
+    # Find all "Opportunity ID's" in the main sheet
+
+    # Update all previous opportunities in the main sheet with new sheet data
+
+    # Insert new opportunities from at the top of the main sheet
