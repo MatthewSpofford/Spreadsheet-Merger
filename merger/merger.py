4f0116da74377ce0b2ada266734c092f144448d5
Matthew Spofford <matthewspofford@verizon.net>
2022-08-17 17:26:47 -0400

    First iteration of update function done

--- merger/merger.py
+++ merger/merger.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional, Dict
+from typing import Optional, Dict, NamedTuple
 
 import openpyxl as pyxl
 
@@ -15,9 +15,9 @@ def merge_spreadsheets(main_file_path, new_file_path):
         pyxl.load_workbook(new_file_path, read_only=True)["Manager_Opportunity_Dashboard"]
 
     # Locate column label positions
-    label_positions = _locate_labels(main_sheet, new_sheet)
-    opp_id_col = label_positions[_OPPORTUNITY_LABEL].new_col
-    opp_id_col_index = opp_id_col - 1
+    label_indices = _locate_labels(main_sheet, new_sheet)
+    opp_id_col_index = label_indices[_OPPORTUNITY_LABEL].new_label_index
+    opp_id_col = opp_id_col_index + 1
 
     # Find all "Opportunity ID's" in the main sheet
     for (new_opp_row,) in new_sheet.iter_rows(min_col=opp_id_col, max_col=opp_id_col, min_row=2):
@@ -30,7 +30,7 @@ def merge_spreadsheets(main_file_path, new_file_path):
             main_opp_row = main_sheet.iter_rows(min_row=2, max_row=2)
 
         # Update all previous opportunities in the main sheet with new sheet data
-        _update_opp_row(main_opp_row, new_opp_row, label_positions)
+        _update_opp_row(main_opp_row, new_opp_row, label_indices)
 
     # TODO REMOVE THIS FOR FINAL VERSION
     # Creates copy
@@ -39,15 +39,14 @@ def merge_spreadsheets(main_file_path, new_file_path):
     main_wb.save(new_file_path)
 
 
-class _LabelColumns:
-    def __init__(self, main_col: int, new_col: Optional[int]):
-        self.main_col = main_col
-        self.new_col = new_col
+class _LabelIndices(NamedTuple):
+    main_label_index: int
+    new_label_index: Optional[int]
 
 
 def _locate_labels(main_sheet: pyxl.workbook.workbook.Worksheet, new_sheet: pyxl.workbook.workbook.Worksheet):
-    """Map column labels used in new sheet to the main sheet positions"""
-    label_positions = {}
+    """Map column label indexes used in new sheet to the main sheet indexes"""
+    label_indices = {}
 
     for (main_label_cell,) in main_sheet.iter_cols(min_col=1, min_row=1, max_row=1):
         new_col = 1
@@ -58,16 +57,17 @@ def _locate_labels(main_sheet: pyxl.workbook.workbook.Worksheet, new_sheet: pyxl
 
             # Keep track of where the label exists in each
             if main_label_cell.value.lower() == new_label_cell.value.lower():
-                label_positions[main_label_cell.value] = _LabelColumns(main_label_cell.column, new_label_cell.column)
+                label_indices[main_label_cell.value] = _LabelIndices(main_label_cell.column - 1,
+                                                                     new_label_cell.column - 1)
                 break
 
             new_col += 1
 
         # Column label in main does not exist in new, and should be ignored later on
-        if main_label_cell.value not in label_positions:
-            label_positions[main_label_cell.value] = _LabelColumns(main_label_cell.column, None)
+        if main_label_cell.value not in label_indices:
+            label_indices[main_label_cell.value] = _LabelIndices(main_label_cell.column, None)
 
-    return label_positions
+    return label_indices
 
 
 def _locate_opp_row(sheet: pyxl.workbook.workbook.Worksheet,
@@ -82,5 +82,10 @@ def _locate_opp_row(sheet: pyxl.workbook.workbook.Worksheet,
     return None
 
 
-def _update_opp_row(main_row, new_row, label_positions: Dict[str, _LabelColumns]):
-    pass
+def _update_opp_row(main_row, new_row, label_indices: Dict[str, _LabelIndices]):
+    for indexes in label_indices.values():
+        # If label does not exist in the new sheet, just ignore this label
+        if not indexes.new_label_index:
+            continue
+
+        main_row[indexes.main_label_index].value = new_row[indexes.new_label_index].value
